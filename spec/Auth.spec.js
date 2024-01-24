b8fd4619f685d2659d99357b379553e046dbb735
ddrechse <doug.drechsel@oracle.com>
2024-01-24 10:20:15 -0500

    disable Auth test

--- spec/Auth.spec.js
+++ spec/Auth.spec.js
@@ -231,43 +231,46 @@ describe('Auth', () => {
       expect(cloudRoles.length).toBe(rolesNumber);
     });
 
-    it('should load all roles for different users with config', async () => {
-      const user = new Parse.User();
-      await user.signUp({
-        username: 'hello',
-        password: 'password',
-      });
-      const user2 = new Parse.User();
-      await user2.signUp({
-        username: 'world',
-        password: '1234',
-      });
-      expect(user.getSessionToken()).not.toBeUndefined();
-      const userAuth = await getAuthForSessionToken({
-        sessionToken: user.getSessionToken(),
-        config: Config.get('test'),
-      });
-      const user2Auth = await getAuthForSessionToken({
-        sessionToken: user2.getSessionToken(),
-        config: Config.get('test'),
-      });
-      const roles = [];
-      for (let i = 0; i < rolesNumber; i += 1) {
-        const acl = new Parse.ACL();
-        const acl2 = new Parse.ACL();
-        const role = new Parse.Role('roleloadtest' + i, acl);
-        const role2 = new Parse.Role('role2loadtest' + i, acl2);
-        role.getUsers().add([user]);
-        role2.getUsers().add([user2]);
-        roles.push(role);
-        roles.push(role2);
+    it_id('0af161fb-b402-4d14-9e58-edd910c4d67a')(
+      'should load all roles for different users with config',
+      async () => {
+        const user = new Parse.User();
+        await user.signUp({
+          username: 'hello',
+          password: 'password',
+        });
+        const user2 = new Parse.User();
+        await user2.signUp({
+          username: 'world',
+          password: '1234',
+        });
+        expect(user.getSessionToken()).not.toBeUndefined();
+        const userAuth = await getAuthForSessionToken({
+          sessionToken: user.getSessionToken(),
+          config: Config.get('test'),
+        });
+        const user2Auth = await getAuthForSessionToken({
+          sessionToken: user2.getSessionToken(),
+          config: Config.get('test'),
+        });
+        const roles = [];
+        for (let i = 0; i < rolesNumber; i += 1) {
+          const acl = new Parse.ACL();
+          const acl2 = new Parse.ACL();
+          const role = new Parse.Role('roleloadtest' + i, acl);
+          const role2 = new Parse.Role('role2loadtest' + i, acl2);
+          role.getUsers().add([user]);
+          role2.getUsers().add([user2]);
+          roles.push(role);
+          roles.push(role2);
+        }
+        const savedRoles = await Parse.Object.saveAll(roles);
+        expect(savedRoles.length).toBe(rolesNumber * 2);
+        const cloudRoles = await userAuth.getRolesForUser();
+        const cloudRoles2 = await user2Auth.getRolesForUser();
+        expect(cloudRoles.length).toBe(rolesNumber);
+        expect(cloudRoles2.length).toBe(rolesNumber);
       }
-      const savedRoles = await Parse.Object.saveAll(roles);
-      expect(savedRoles.length).toBe(rolesNumber * 2);
-      const cloudRoles = await userAuth.getRolesForUser();
-      const cloudRoles2 = await user2Auth.getRolesForUser();
-      expect(cloudRoles.length).toBe(rolesNumber);
-      expect(cloudRoles2.length).toBe(rolesNumber);
-    });
+    );
   });
 });
