4bbe232fecfa31fc11ee293af0d3e399dba49214
ddrechse <doug.drechsel@oracle.com>
2024-02-22 08:36:18 -0500

    Original set of tests for SEGV

--- spec/AudienceRouter.spec.js
+++ spec/AudienceRouter.spec.js
@@ -317,8 +317,7 @@ describe('AudiencesRouter', () => {
     );
   });
 
-  it_exclude_dbs(['postgres'])(
-    //  it_exclude_dbs(['postgres', 'oracle'])(
+  it_exclude_dbs(['postgres', 'oracle'])(
     'should support legacy parse.com audience fields',
     done => {
       const database = Config.get(Parse.applicationId).database.adapter.database;
