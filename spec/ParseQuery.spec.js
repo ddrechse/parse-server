60c6039744e0c3d0a9c3aba32b585d97b341ec17
ddrechse <doug.drechsel@oracle.com>
2024-02-23 12:15:50 -0500

    Investigate test failure

--- spec/ParseQuery.spec.js
+++ spec/ParseQuery.spec.js
@@ -1006,7 +1006,7 @@ describe('Parse.Query testing', () => {
       });
   });
 
-  it('equalTo queries', function (done) {
+  fit('equalTo queries', function (done) {
     const makeBoxedNumber = function (i) {
       return new BoxedNumber({ number: i });
     };
